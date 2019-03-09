1717c8d18c5fdedde95ba662024b91308d44badb
Ville Seppänen <ville.seppanen@gofore.com>
2019-03-09 16:24:17 +0200

    Draw map based on pathing grid

--- example-bot/bot/util/state_sender.py
+++ example-bot/bot/util/state_sender.py
@@ -5,6 +5,21 @@ import json
 class StateSender:
     def __init__(self, bot):
         self.bot = bot
+        self.map = self._get_map()
+
+    def _get_map(self):
+        blocked_points = []
+        grid = self.bot.game_info.pathing_grid
+        print(grid.print())
+        for y in range(grid.height):
+            for x in range(grid.width):
+                if grid.is_set((x, y)):
+                    blocked_points.append({
+                        "x": x,
+                        "y": y
+                    })
+        return blocked_points
+
 
     async def _send_websocket_message(self, message):
         async with websockets.connect('ws://localhost:9000') as websocket:
@@ -30,7 +45,8 @@ class StateSender:
                     "pos_x": unit.position.x,
                     "pos_y": unit.position.y,
                     "is_visible": unit.is_visible
-                } for unit in enemy_units]
+                } for unit in enemy_units],
+            "map": self.map
         }
 
         await self._send_websocket_message(json.dumps(payload))
